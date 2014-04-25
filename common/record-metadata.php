ac3aaa80615e01e8f28b92cb19e64b246c5bcea0
Tom Elliott <tom.elliott@nyu.edu>
2014-04-25 16:45:39 -0500

    adding an override for the record metadata display; written by patrickmj and snagged from http://omeka.org/forums/topic/customizing-item-output

---
+++ common/record-metadata.php
@@ -0,0 +1,44 @@
+<?php if(isset(get_view()->item)): //check if this looks like an item show page ?>
+
+<?php
+//dig through the elements for display that are passed into this file
+//put it all into a new array of just the elements we want
+//this should let you collect the elements you want in the order you want
+//follow this pattern to get more or change the order
+
+$wantedElements = array();
+$wantedElements['Title'] = $elementsForDisplay['Dublin Core']['Title'];
+if(isset($elementsForDisplay['Item Type Metadata'])) {
+    $wantedElements['Primary Source Text'] = $elementsForDisplay['Item Type Metadata']['Text'];
+}
+$wantedElements['Commentary'] = $elementsForDisplay['Dublin Core']['Description'];
+?>
+
+<div class="element-set">
+    <?php foreach ($wantedElements as $elementName => $elementInfo): ?>
+    <div id="<?php echo text_to_id(html_escape("$elementName")); ?>" class="element">
+        <h3><?php echo html_escape(__($elementName)); ?></h3>
+        <?php foreach ($elementInfo['texts'] as $text): ?>
+            <div class="element-text"><?php echo $text; ?></div>
+        <?php endforeach; ?>
+    </div><!-- end element -->
+    <?php endforeach; ?>
+</div><!-- end element-set -->
+
+<?php else: ?>
+
+<?php foreach ($elementsForDisplay as $setName => $setElements): ?>
+<div class="element-set">
+    <h2><?php echo html_escape(__($setName)); ?></h2>
+    <?php foreach ($setElements as $elementName => $elementInfo): ?>
+    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+        <h3><?php echo html_escape(__($elementName)); ?></h3>
+        <?php foreach ($elementInfo['texts'] as $text): ?>
+            <div class="element-text"><?php echo $text; ?></div>
+        <?php endforeach; ?>
+    </div><!-- end element -->
+    <?php endforeach; ?>
+</div><!-- end element-set -->
+<?php endforeach; ?>
+
+<?php endif; ?>
