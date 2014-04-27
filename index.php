5f0c8d855b199eb869260b17bd48f31a54644065
Tom Elliott <tom.elliott@nyu.edu>
2014-04-27 06:20:00 -0500

    swap primary and secondary content divisions, so recent items are on the left and text on the right

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
 <div id="primary">
-    <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
-    <p><?php echo $homepageText; ?></p>
-    <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
-    <div id="featured-item">
-        <h2><?php echo __('Featured Item'); ?></h2>
-        <?php echo random_featured_items(1); ?>
-    </div><!--end featured-item-->
-    <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Collection')): ?>
-    <!-- Featured Collection -->
-    <div id="featured-collection">
-        <h2><?php echo __('Featured Collection'); ?></h2>
-        <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
-    <?php endif; ?>
-    <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-    <!-- Featured Exhibit -->
-    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
-
-</div><!-- end primary -->
-
-<div id="secondary">
     <!-- Recent Items -->
     <div id="recent-items">
         <h2><?php echo __('Recently Added Items'); ?></h2>
@@ -51,6 +26,33 @@
         <?php endif; ?>
     </div><!-- end recent-items -->
     
+
+
+</div><!-- end primary -->
+
+<div id="secondary">
+    <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
+    <p><?php echo $homepageText; ?></p>
+    <?php endif; ?>
+    <?php if (get_theme_option('Display Featured Item') == 1): ?>
+    <!-- Featured Item -->
+    <div id="featured-item">
+        <h2><?php echo __('Featured Item'); ?></h2>
+        <?php echo random_featured_items(1); ?>
+    </div><!--end featured-item-->
+    <?php endif; ?>
+    <?php if (get_theme_option('Display Featured Collection')): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+    <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <!-- Featured Exhibit -->
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+    <?php endif; ?>
+
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
 </div><!-- end secondary -->
